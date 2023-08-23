ccbb2bd4958095e7d42f541992e95e066517bd60
icp049 <ianpepe2000@gmail.com>
2023-08-23 01:56:22 -0600

    reinvented navbar

--- src/navbars/Landingpagenavbar.jsx
+++ src/navbars/Landingpagenavbar.jsx
@@ -1,64 +1,43 @@
-import { Box, IconButton } from '@mui/material';
-import {
-  
-  SearchOutlined,
-  ExitToAppOutlined, // 1. Import the ExitToAppOutlined icon
-} from '@mui/icons-material';
-import { Link } from 'react-router-dom';
-import styled from '@emotion/styled';
-
+import React, { useState } from 'react';
+import { FaBars, FaTimes } from 'react-icons/fa';
+import styles from './Landingpagenavbar.module.css';
 
 const Landingpagenavbar = () => {
+  const [click, setClick] = useState(false);
 
+  const handleClick = () => setClick(!click);
+  const closeMenu = () => setClick(false);
 
   return (
-    <Box
-      display="flex"
-      alignItems="center"
-      width="100%"
-      height="60px"
-      backgroundColor="grey"
-      color="black"
-      position="fixed"
-      top="0"
-      left="0"
-      zIndex="2"
-    >
-      <Box
-        width="80%"
-        margin="auto"
-        display="flex"
-        justifyContent="space-between"
-        alignItems="center"
-      >
-        <Box
-          sx={{ ':hover': { cursor: "pointer" } }}
-          color="blue"
-        >
-          TosmosListings
-        </Box>
-
-        <Box
-          display="flex"
-          justifyContent="space-between"
-          columnGap="20px"
-          zIndex="2"
-        >
-          <Link to="">My Profile</Link>
-          <Link to="">Messages</Link>
-          <Link to="">Listings</Link>
-
-          <IconButton sx={{ color: "black" }}>
-            <SearchOutlined />
-          </IconButton>
-
-          {/* Logout Button */}
-          <IconButton sx={{ color: "black" }} >
-            <ExitToAppOutlined />
-          </IconButton>
-        </Box>
-      </Box>
-    </Box>
+    <div className={styles.header}>
+      <nav className={styles.navbar}>
+        
+        <div className={styles.hamburger} onClick={handleClick}>
+          {click ? (
+            <FaTimes size={30} style={{ color: '#ffffff' }} />
+          ) : (
+            <FaBars size={30} style={{ color: '#ffffff' }} />
+          )}
+        </div>
+        <ul className={click ? `${styles['nav-menu']} ${styles.active}` : styles['nav-menu']}>
+          <li className={styles['nav-item']}>
+            <a href='/' onClick={closeMenu}>Home</a>
+          </li>
+          <li className={styles['nav-item']}>
+            <a href='#about' onClick={closeMenu}>About</a>
+          </li>
+          <li className={styles['nav-item']}>
+            <a href='#projects' onClick={closeMenu}>Projects</a>
+          </li>
+          <li className={styles['nav-item']}>
+            <a href='#resume' onClick={closeMenu}>Online Resume</a>
+          </li>
+          <li className={styles['nav-item']}>
+            <a href='#imahe' onClick={closeMenu}>Imahe</a>
+          </li>
+        </ul>
+      </nav>
+    </div>
   );
 };
 
